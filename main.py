dd00944c560ef6e9bf283511256c45ca333c0784
iconti14 <izzyconti14@gmail.com>
2024-07-17 18:38:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import asyncio
 import os
-import random 
+import random
 import json
 
 from discord.ext import commands
@@ -25,6 +25,7 @@ with open("configs/messages.json", encoding="utf-8") as fh:
 with open("configs/commands.json", encoding="utf-8") as fh:
     bot_commands = json.load(fh)
 
+
 def select_image(folderName: str):
     """
     Selects a random photo from photo bank and returns it to be sent
@@ -36,11 +37,11 @@ def select_image(folderName: str):
     folderName = folderName.lower()
     pathToPhotos = os.path.join("pics", folderName)
 
-    if folderName not in pictureFolderNames:
-        return None
-
     if folderName == "list":
         return "list"
+
+    if folderName not in pictureFolderNames:
+        return None
 
     listofAvailablePics = os.listdir(pathToPhotos)
     return os.path.join(
@@ -65,49 +66,53 @@ async def on_message(message):
         text = message.content.split("$")
         spaceSplit = text[1].split(" ")
 
-        if spaceSplit[0] in bot_commands.keys():
+        if spaceSplit[0] in bot_commands:
 
             # reply functionality
             if bot_commands[spaceSplit[0]]["func"] == "reply":
                 await message.reply(bot_commands[spaceSplit[0]]["reply"])
-            
+
             # Sendpic functionality
             elif bot_commands[spaceSplit[0]]["func"] == "sendpic":
                 selectImage = select_image(spaceSplit[1])
-                if selectImage == None:
+                if selectImage is None:
                     await message.reply("That's not an option, idiot 😡")
-                elif selectImage[0] == "list":
+                elif selectImage == "list":
                     await message.reply(
-                        "List of folders is: {}".format(pictureFolderNames))    
-                elif type(selectImage[0]) == str:
+                        "List of folders is: {}".format(pictureFolderNames))
+                elif isinstance(selectImage, str):
                     await message.reply("Here is a {} picture".format(
-                            spaceSplit[1]),file=discord.File(selectImage))
+                        spaceSplit[1]),
+                                        file=discord.File(selectImage))
 
     # These are not commands, just silly things
-    for key in bot_messages.keys():
+    for key in bot_messages:
         if key in message.content:
             falseTrigger = True
-            if "special" not in bot_messages[key]:
+            if "special" not in bot_messages[key] or (
+                    bot_messages[key]["special"] == "endswith"
+                    and message.content[-1] == key and key
+                    not in message.content.rstrip(message.content[-1])):
                 falseTrigger = False
-            elif bot_messages[key]["special"] == "endswith":
-                if message.content[-1] == key:
-                    falseTrigger = False
 
             if not falseTrigger:
                 if bot_messages[key]["type"] == "message":
                     await message.reply(bot_messages[key]["reply"])
                 elif bot_messages[key]["type"] == "image":
-                    await message.reply(file=discord.File(bot_messages[key]["imagePath"]))
-                elif bot_messages[key]["type"] == "reactions":
+                    await message.reply(
+                        file=discord.File(bot_messages[key]["imagePath"]))
+                elif bot_messages[key]["type"] == "react":
                     for value in bot_messages[key]["reactions"]:
                         await message.add_reaction(value)
 
+
 @bot.event
 async def on_message_edit(messageBefore, messageAfter):
-    await messageAfter.reply("I SAW THAT 👀")
+    if messageBefore.content != messageAfter.content:
+        await messageAfter.reply("I SAW THAT 👀")
 
 
 if TOKEN is None:
     print("DISCORD_TOKEN environment variable is not set. Please set it.")
-else: 
+else:
     bot.run(TOKEN)
